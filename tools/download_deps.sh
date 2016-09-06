9810f343724e4b240a2c63bf0d309fb8c583dc8e
James Batt <place.net.au@gmail.com>
2016-09-06 04:15:40 -0700

    added qemu to package.sh

--- tools/download_deps.sh
+++ tools/download_deps.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-curl -L -o http://wiki.qemu-project.org/download/qemu-2.7.0.tar.bz2
-
-
 curl -L -o binutils-2.26.tar.gz binutils-2.26.tar.gz ftp://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz
 tar xvzf binutils-2.26.tar.gz
 
--- tools/packages.sh
+++ tools/packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+sudo apt-get install -y qemu-system-i386
 sudo apt-get install -y libgmp3-dev
 sudo apt-get install -y libmpfr-dev
 sudo apt-get install -y libisl-dev
